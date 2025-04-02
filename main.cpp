e0f5cf8dfcd0461db1ca4bb6c009921f71041448
Alex-f98 <72577795+Alex-f98@users.noreply.github.com>
2025-04-02 15:40:14 -0300

    Add files via upload
    Cargo scripts generales

---
+++ main.cpp
@@ -0,0 +1,34 @@
+
+/*
+ * @file   main.cpp
+ * @author Brian Fuentes (bfuentes@fi.uba.ar)
+ * @brief  codigo principal, modelo general
+ * @version 0.1
+ * @date 2025-04-02
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+//=====[Libraries]=============================================================
+#include "mbed.h"
+#include "arm_book_lib.h"
+
+//=====[Defines]===============================================================
+
+//=====[Declaration and initialization of public global objects]===============
+
+
+//=====[Declaration and initialization of public global variables]===========
+
+
+//=====[Main function, the program entry point after power on or reset]========
+int main()
+{
+
+   //----Codigo aqui---
+
+   return 0;
+}
+
+//=====[Implementations of public functions]===================================
